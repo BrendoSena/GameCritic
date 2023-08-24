509cc57404171ae22f53afa337ba94376bfe4286
Brendo Sena de Souza <brendosenadesouza@gmail.com>
2023-08-23 22:15:55 -0300

    NovosConteudos

--- logica.js
+++ logica.js
@@ -24,3 +24,34 @@ function menuToggle() {
 	}
 }
 
+
+const contJogos = [
+  document.getElementById('contJogos0'),
+  document.getElementById('contJogos1'),
+  document.getElementById('contJogos2'),
+  document.getElementById('contJogos3')
+];
+
+let contador = 0;
+
+function proximo() {
+  contJogos[contador].style.display = 'none';
+  contador = (contador + 1) % contJogos.length;
+  exibirConteinerAtual();
+}
+
+function voltar() {
+  contJogos[contador].style.display = 'none';
+  contador = (contador - 1 + contJogos.length) % contJogos.length;
+  exibirConteinerAtual();
+}
+
+function exibirConteinerAtual() {
+  if (contador >= 3 && contJogos[3].style.display === 'none') {
+    contador = 0;
+  }
+
+  for (let i = 0; i < contJogos.length; i++) {
+    contJogos[i].style.display = i === contador ? 'flex' : 'none';
+  }
+}
